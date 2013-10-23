797aef3e05884c2d7201b05d23c6e2995f39ec1c
bright-tools <dev@brightsilence.com>
2013-10-24 00:49:44 +0100

    New test output file

---
+++ tests/switch-1.c
@@ -0,0 +1,45 @@
+/*
+   Part of the Bright coverage tool test suite
+
+   Copyright 2013 John Bailey
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+int
+main (void)
+{
+  int i;
+  int t;
+
+  switch( i )
+  {
+      case 0:
+          t = 1;
+          break;
+      case 1:
+          if( i == 1 )
+              t = 2;
+          else
+              t = 3;
+          break;
+      case 2:
+          t = 4;
+          /* Fall-through */
+      default:
+          t = 5;
+          break;
+  }
+
+  return 0;
+}
